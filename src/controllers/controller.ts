ee0d2eab49e92bc37202f4b9da04c81d6c79fbdf
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-22 13:20:31 -0300

    CRUD com mongoose

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -11,5 +11,13 @@ export class Controller {
 
     public routes() {
         this.app.route('/').get(this.apiService.welcomeMessage);
+
+        this.app.route('/pokemons').get(this.apiService.getAllPokemon);
+
+        this.app.route("/pokemon").post(this.apiService.addNewPokemon);
+
+        this.app.route("/pokemon/:id").delete(this.apiService.deletePokemon);
+
+        this.app.route("/pokemon/:id").put(this.apiService.updatePokemon);
     }
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,60 @@
 import { Request, Response } from "express";
 import { Pokemon } from '../models/pokemon.models';
+import { MongooseDocument } from "mongoose";
 
 export class ApiService {
     public welcomeMessage(req: Request, res: Response) {
         return res.status(200).send("Welcome to API rest by Kaio");
     }
+
+    public getAllPokemon(req: Request, res: Response) {
+        Pokemon.find({}, (error: Error, pokemon: MongooseDocument) => {
+            if(error) {
+                res.send(error);
+            }
+            res.json(pokemon);
+        })
+    }
+
+    public addNewPokemon(req: Request, res: Response) {
+        const newPokemon = new Pokemon(req.body);
+        newPokemon.save((error: Error, pokemon: MongooseDocument) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.json(pokemon);
+            }
+        })
+    }
+
+
+    public deletePokemon(req: Request, res: Response) {
+        const pokemonID = req.params.id;
+        Pokemon.findByIdAndDelete(pokemonID, (error: Error, deleted: any) => {
+            if (error) {
+                res.send(error);
+            } 
+            const message = deleted ? 'Deleted successfully' : 'Pokemon not found';
+            res.send(message);
+        });
+    }
+
+    public updatePokemon(req: Request, res: Response) {
+        const pokemonID = req.params.id;
+        Pokemon.findByIdAndUpdate(
+            pokemonID, 
+            req.body,
+            (error: Error, pokemon: any) => {
+                if (error) {
+                    res.send(error);
+                }
+                const message = pokemon
+                    ? 'Updated successfully'
+                    : 'Pokemon not found';
+
+                res.send(message);
+            }
+        )
+    }
+
 }
